2b75b50e9d830f979a72a943101b06520bb980ef
mrbartchak <matthewbartchak@gmail.com>
2025-03-13 23:29:00 -0400

    tbd matchup skeleton

--- src/components/bracket/TeamCard.tsx
+++ src/components/bracket/TeamCard.tsx
@@ -1,5 +1,6 @@
 import { Team } from "@/types/bracket";
 import Image from "next/image";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface TeamCardProps {
   team: Team | null;
@@ -7,27 +8,31 @@ interface TeamCardProps {
 
 export function TeamCard({ team: Team }: TeamCardProps) {
   return (
-    <main className="flex flex-row items-center p-2 space-x-2 hover:bg-gray-100 rounded">
-      {Team?.logo && (
+    <main className="flex flex-row items-center p-1 space-x-2 hover:bg-gray-100 rounded">
+      {Team?.logo ? (
         <Image
-          src={Team?.logo}
-          alt={Team?.name ?? "TBD"}
-          className="w-6 h-auto"
+          src={Team.logo}
+          alt={Team.name ?? "TBD"}
+          className="w-6 h-auto my-1 ml-1 mr-3"
           width={32}
           height={32}
         />
+      ) : (
+        <Skeleton className="w-6 h-6 my-1 ml-1 mr-3 rounded-full" />
       )}
+
       <p
         className="text-xs opacity-50 w-2"
       >
-        {Team?.seed}
+        {Team?.seed ?? "-"}
       </p>
+
       <p
-        className="font-bold text-xs"
+        className="font-semibold text-xs"
         style={{ color: Team ? Team.details?.color ?? "black" : "lightgray" }}
       >
         {Team?.name ?? "TBD"}
-      </p>
+      </p>  
     </main>
   );
 }
