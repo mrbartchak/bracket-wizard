4b1a5a17dc3dfd4028ba33a01bd9ac219f5922e3
mrbartchak <matthewbartchak@gmail.com>
2025-03-14 02:23:51 -0400

    chip card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { RoundHeaderCard } from "@/components/bracket/RoundHeaderCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen max-w-[1536px] mx-auto p-4 space-y-8 border-4">
+    <div className="flex flex-col items-center justify-center min-h-screen max-w-[1536px] mx-auto p-4 space-y-8">
       <RoundHeaderCard />
       <MarchMadnessBracket />
     </div>
--- src/components/bracket/ChampionshipCard.tsx
+++ src/components/bracket/ChampionshipCard.tsx
@@ -6,6 +6,8 @@ import {
   CardTitle
 } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
 
 interface ChampionshipCardProps {
   matchup: Matchup | null;
@@ -19,18 +21,46 @@ export function ChampionshipCard({ matchup }: ChampionshipCardProps) {
           <CardTitle className="text-bold text-xl text-center">
             National Championship
           </CardTitle>
+          <Separator />
         </CardHeader>
-        <Separator />
-        <CardContent className="flex flex-row justify-between space-x-4">
+        <CardContent className="flex flex-row items-center justify-between space-x-4">
           <div className="flex flex-1 flex-row items-center space-x-4">
-            <p>{matchup?.team1?.name}</p>
+            {matchup?.team1?.logo ? (
+              <Image 
+                src={matchup.team1.logo} 
+                alt={`${matchup.team1.name} logo`} 
+                width={32} 
+                height={32} 
+              />
+            ) : (
+              <Skeleton className="h-8 w-8 rounded-full" />
+            )}
+            {matchup?.team1?.name ? (
+              <p>{matchup.team1.name}</p>
+            ) : (
+              <Skeleton className="h-6 w-24" />
+            )}
           </div>
           <Separator orientation="vertical" />
           <div className="flex flex-1 flex-row justify-end items-center space-x-4">
-            <p>{matchup?.team2?.name}</p>
+            {matchup?.team2?.name ? (
+              <p>{matchup.team2.name}</p>
+            ) : (
+              <Skeleton className="h-6 w-24" />
+            )}
+            {matchup?.team2?.logo ? (
+              <Image 
+                src={matchup.team2.logo} 
+                alt={`${matchup.team2.name} logo`} 
+                width={32} 
+                height={32} 
+              />
+            ) : (
+              <Skeleton className="h-8 w-8 rounded-full" />
+            )}
           </div>
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
