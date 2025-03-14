694d070ab8b4849124ac57c7c1c7f7ec05b671cc
mrbartchak <matthewbartchak@gmail.com>
2025-03-14 03:06:17 -0400

    team select starts

--- src/components/bracket/TeamCard.tsx
+++ src/components/bracket/TeamCard.tsx
@@ -4,15 +4,27 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 interface TeamCardProps {
   team: Team | null;
+  isSelected?: boolean;
+  onSelect?: (team: Team | null) => void;
 }
 
-export function TeamCard({ team: Team }: TeamCardProps) {
+export function TeamCard({ team, isSelected, onSelect }: TeamCardProps) {
+  const handleClick = () => {
+    if (team && onSelect) {
+      onSelect(team);
+    }
+  };
+
   return (
-    <main className="flex flex-row items-center h-10 p-1 space-x-2 hover:bg-gray-100 rounded">
-      {Team?.logo ? (
+    <main
+      className={`flex flex-row items-center h-10 p-1 space-x-2 rounded
+        ${isSelected ? "bg-blue-100" : "hover:bg-gray-100"}`}
+      onClick={handleClick}
+    >
+      {team?.logo ? (
         <Image
-          src={Team.logo}
-          alt={Team.name ?? "TBD"}
+          src={team.logo}
+          alt={team.name ?? "TBD"}
           className="w-6 h-auto my-1 ml-1 mr-3"
           width={32}
           height={32}
@@ -21,16 +33,16 @@ export function TeamCard({ team: Team }: TeamCardProps) {
         <Skeleton className="w-6 h-6 my-1 ml-1 mr-3 rounded-full" />
       )}
 
-      {Team ? (
+      {team ? (
         <>
           <p className="text-xs opacity-50 w-2">
-        {Team.seed ?? "-"}
+        {team.seed ?? "-"}
           </p>
           <p
         className="font-semibold text-xs"
-        style={{ color: Team.details?.color ?? "black" }}
+        style={{ color: team.details?.color ?? "black" }}
           >
-        {Team.name}
+        {team.name}
           </p>
         </>
       ) : (
